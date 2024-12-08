435fb5f7570ae64f6eb746659a421219bc1fd823
VlJulia <j.v.04@bk.ru>
2024-12-08 07:39:42 +0300

    все нажатия работают

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import liked from './images/liked.svg';
 import not_liked from './images/like.svg';
 import image_tmpl from './images/card-templ.jpg';
 import trash from './images/trash.svg';
-console.log('Hello, World!');
-const numbers = [2, 3, 5];
-let  del_card;
+const cards = document.querySelectorAll('.card');
 const overlay = document.querySelector('.overlay');
+const modal_new_card = document.querySelector(".new-card");
+const modal_edit_profile = document.querySelector(".profile-edit");
+const modal_new_avatar = document.querySelector(".new-avatar");
+let  del_card;
+
 
 function hideOverlay(){
+    if (!overlay.classList.contains('show')) return;
     overlay.classList.remove('show');
     const modal = overlay.querySelector('.show');
     modal.classList.remove('show');
@@ -27,12 +31,7 @@ overlay.querySelector(".modal").addEventListener('click', function(event) {
     event.stopPropagation();
 });
 
-// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
-const doubledNumbers = numbers.map(number => number * 2);
 
-console.log(doubledNumbers); // 4, 6, 10
-
-const cards = document.querySelectorAll('.card');
 
 function newCard(img_url, text){
     var card = document.createElement('article');
@@ -135,19 +134,20 @@ cards.forEach(card => {
     });
 
 });
-document.querySelector('.add-button').addEventListener('click', function() {
-    const card = newCard("https://www.aqua-shop.ru/images/news/nerazluchniki.jpg", "AAAAAAA");
-    document.querySelector('.content').appendChild(card);
-});
 
-document.querySelector('.avatar-hover').addEventListener('click', function(){
+
+document.querySelector('.add-button').addEventListener('click', function(event) {
+    event.stopPropagation();
+    showOverlay("new-card");
+});
+document.querySelector('.avatar-hover').addEventListener('click', function(event){
+    event.stopPropagation();
     showOverlay('new-avatar');
 });
-
-document.querySelector('.edit-button').addEventListener('click', function(){
+document.querySelector('.edit-button').addEventListener('click', function(event){
+    event.stopPropagation();
     showOverlay('profile-edit');
 });
-
 document.querySelector('.add-button').addEventListener('click', function(){
     showOverlay('new-card');
 });
@@ -157,3 +157,84 @@ document.querySelector(".are-you-sure").querySelector(".modal_button").addEventL
     }
     hideOverlay();
 });
+
+modal_new_card.querySelector(".modal_button").addEventListener('click', function(event){
+    event.preventDefault();
+    const text =  modal_new_card.querySelector("input[name='card-name']").value;
+    const img =  modal_new_card.querySelector("input[name='card-img-url']").value;
+    const new_card = newCard(img, text);
+    document.querySelector('.content').appendChild(new_card);
+    hideOverlay();
+});
+modal_edit_profile.querySelector(".modal_button").addEventListener('click', function(event){
+    event.preventDefault();
+    const text =  modal_edit_profile.querySelector("input[name='profile-name']").value;
+    const about =  modal_edit_profile.querySelector("input[name='profile-about-self']").value;
+    document.querySelector('#profile-name').textContent=text;
+    document.querySelector('#profile-about-self').textContent=about;
+    if (document.querySelector('.profile').classList.contains('profile-open')) {openHeader();}
+    if (canOpenHeader()) {
+        document.querySelector('.profile').addEventListener('click', openHeader);
+        document.querySelector('.profile').style.cursor ="pointer";
+    }
+    else{
+        document.querySelector('.profile').removeEventListener ('click', openHeader);
+        document.querySelector('.profile').style.cursor ="unset";
+    }
+    hideOverlay();
+});
+modal_new_avatar.querySelector(".modal_button").addEventListener('click', function(event){
+    event.preventDefault();
+    const img =  modal_new_avatar.querySelector("input[name='avatar-img-url']").value;
+    document.querySelector('#profile-avatar').src=img;
+    hideOverlay();
+});
+function checkInputs(inputs,submitButton) {
+    const allFilled = Array.from(inputs).every(input => input.checkValidity());
+    // Устанавливаем состояние кнопки
+    submitButton.disabled = !allFilled;
+}
+
+Array.from(document.querySelector(".modal").children).forEach((modal)=>{
+    const inputs = modal.querySelectorAll('input:not([type="submit"]');
+    if(inputs){
+        const submit = modal.querySelector('.modal_button');
+        inputs.forEach((inp)=>{
+            inp.addEventListener('input', function(){
+                const status = inp.checkValidity();
+                if (!status && inp.value!=""){
+                    if (!inp.parentElement.classList.contains("error")) inp.parentElement.classList.toggle("error");
+                }
+                else if (inp.parentElement.classList.contains("error")) inp.parentElement.classList.toggle("error");
+                checkInputs(inputs, submit);
+                });
+        });
+    }
+})
+
+
+document.addEventListener('keyup',function (evt)
+ {if (evt.key === "Escape") {hideOverlay();}
+}
+);
+function canOpenHeader(){
+    const element1 = document.querySelector('.description');
+    const element2= document.querySelector('.profile-name');
+    return !((element1.scrollWidth <= element1.clientWidth)&&(element2.scrollWidth <= element2.clientWidth)) ;
+};
+
+function openHeader(){
+    if (document.querySelector('.profile').classList.contains('profile-open')) {
+        document.querySelector('.profile').classList.remove('profile-open');
+        setTimeout(() => {
+            document.querySelector('.profile').classList.remove('profile-column');
+        },1000);
+        return;
+    }
+    document.querySelector('.profile').classList.add('profile-hide');
+    setTimeout(() => {
+        document.querySelector('.profile').classList.add('profile-column');
+        document.querySelector('.profile').classList.remove('profile-hide');
+        document.querySelector('.profile').classList.add('profile-open');
+    },500);
+};
